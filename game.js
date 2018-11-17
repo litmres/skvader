0ec4b53fe61e2a69925bf4174dacc10b57b9049e
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-11-17 14:40:45 -0800

    feat(map): initial map Procedurally generated map (with no walls)

---
+++ game.js
@@ -0,0 +1,33 @@
+var Game = {
+    display: null,
+    map: {},
+
+    init: function() {
+        this.display = new ROT.Display();
+        document.body.appendChild(this.display.getContainer());
+        this._generateMap();
+    },
+
+    _generateMap: function() {
+        var digger = new ROT.Map.Digger();
+
+        var digCallback = function(x, y, value) {
+            if (value) { return; } /* do not store walls */
+
+            var key = x+","+y;
+            this.map[key] = ".";
+        };
+        digger.create(digCallback.bind(this));
+        this._drawWholeMap();
+        console.log(this.map);
+    },
+
+    _drawWholeMap: function() {
+        for (var key in this.map) {
+            var parts = key.split(",");
+            var x = parseInt(parts[0]);
+            var y = parseInt(parts[1]);
+            this.display.draw(x, y, this.map[key]);
+        }
+    }
+}
