f5a5be902199d6102b1f9c8813c17c1a5daa7116
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-11-17 14:51:20 -0800

    feat(map): adding boxes to the map Tidying up the JS

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var Game = {
+const Game = {
     display: null,
     map: {},
 
@@ -9,29 +9,39 @@ var Game = {
     },
 
     _generateMap: function() {
-        var digger = new ROT.Map.Digger();
+        const digger = new ROT.Map.Digger();
+        const freeCells = [];
 
-        var digCallback = function(x, y, value) {
-            var key = x+","+y;
+        const digCallback = function(x, y, value) {
+            const key = x+","+y;
 
             if (value) {
                 // Not floor tile i.e. a wall
                 this.map[key] = "%";
             } else {
                 this.map[key] = ".";
+                freeCells.push(key);
             }
         };
         digger.create(digCallback.bind(this));
+        this._generateBoxes(freeCells);
         this._drawWholeMap();
-        console.log(this.map);
     },
 
     _drawWholeMap: function() {
-        for (var key in this.map) {
-            var parts = key.split(",");
-            var x = parseInt(parts[0]);
-            var y = parseInt(parts[1]);
+        for (let key in this.map) {
+            const parts = key.split(",");
+            const x = parseInt(parts[0]);
+            const y = parseInt(parts[1]);
             this.display.draw(x, y, this.map[key]);
+        }
+    },
+
+    _generateBoxes: function(freeCells) {
+        for (let i=0;i<10;i++) {
+            const index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+            const key = freeCells.splice(index, 1)[0];
+            this.map[key] = "*";
         }
     }
 }
