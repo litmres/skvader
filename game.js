4c535355cfee56efa444b5b962b065f1be5e8fea
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-11-17 15:32:09 -0800

    feat(engine): adding a simple game engine

--- game.js
+++ game.js
@@ -2,11 +2,18 @@ const Game = {
     display: null,
     map: {},
     player: null,
+    engine: null,
 
     init: function() {
         this.display = new ROT.Display();
         document.body.appendChild(this.display.getContainer());
         this._generateMap();
+
+        const scheduler = new ROT.Scheduler.Simple();
+        scheduler.add(this.player, true);
+
+        this.engine = new ROT.Engine(scheduler);
+        this.engine.start();
     },
 
     _generateMap: function() {
@@ -66,3 +73,39 @@ const Player = function(x, y) {
 Player.prototype._draw = function() {
     Game.display.draw(this._x, this._y, "@", "#ff0");
 };
+
+Player.prototype.act = function() {
+    Game.engine.lock();
+    /* wait for user input; do stuff when user hits a key */
+    window.addEventListener("keydown", this);
+}
+
+Player.prototype.handleEvent = function(e) {
+    var keyMap = {};
+    keyMap[38] = 0;
+    keyMap[33] = 1;
+    keyMap[39] = 2;
+    keyMap[34] = 3;
+    keyMap[40] = 4;
+    keyMap[35] = 5;
+    keyMap[37] = 6;
+    keyMap[36] = 7;
+
+    var code = e.keyCode;
+    /* one of numpad directions? */
+    if (!(code in keyMap)) { return; }
+
+    /* is there a free space? */
+    var dir = ROT.DIRS[8][keyMap[code]];
+    var newX = this._x + dir[0];
+    var newY = this._y + dir[1];
+    var newKey = newX + "," + newY;
+    if (!(newKey in Game.map)) { return; }
+
+    Game.display.draw(this._x, this._y, Game.map[this._x+","+this._y]);
+    this._x = newX;
+    this._y = newY;
+    this._draw();
+    window.removeEventListener("keydown", this);
+    Game.engine.unlock();
+}
