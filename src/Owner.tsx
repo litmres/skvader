5d7a39ad8978f61d08a59700951d64de28f5521d
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-11-25 23:02:40 -0800

    feat(intro): adding speech to the owner's speech bubble

--- src/Owner.tsx
+++ src/Owner.tsx
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
 
+interface TextStateProps { speech :  string[] }
+
 /**
  * Original Ascii art by Art by Anthony Thyssen: https://www.asciiart.eu/people/faces
  */
-export class Owner extends Component{
-    private readonly asciiOwner = [
+export class Owner extends Component<TextStateProps>{
+    private readonly asciiOwner: string[] = [
         "                           ;;;;;;;;;",
         "                        ;;;;;;;;;;;;;",
         "                   ;;;;;;;;;;;;;;;;;",
@@ -13,13 +15,13 @@ export class Owner extends Component{
         "              ;;;;;      ;;          |;",
         "             ;;;;         ;          |",
         "             ;;;                     |",
-        "              ;;                     )      .............................................",
+        "              ;;                     )      ............................................",
         "               \\    ~~~~ ~~~~~~~    /       . ",
         "                \\    ~~~~~~~  ~~   /        . ",
         "              |\\ \\                / /|      . ",
-        "               \\\\| %%%%%    %%%%% |//       .............................................",
+        "               \\\\| %%%%%    %%%%% |//       ............................................",
         "              [[====================]]      ........",
-        "               | |  ^   |   |  ^   | |     ......",
+        "               | |  ^   |   |   ^  | |     ......",
         "               | | :@:  |   |  :@: | |   ..... ",
         "                \\______ /   \\_______/  ....",
         "                 |     (@\\/@)     |  ...",
@@ -39,9 +41,22 @@ export class Owner extends Component{
         ""
     ];
     private readonly textStartRow = 9;
-    private readonly textStartColumn = 47;
     private readonly maxCharacterNum = 40;
+
+    /**
+     *
+     * @param speechText
+     */
+    private padTextForSpeechBubble(speechText: string): string {
+        return speechText.padEnd(this.maxCharacterNum, " ");
+    }
+    private addSpeechText(ownerAscii: string[], speechText: string[]) {
+        ownerAscii[this.textStartRow] = ownerAscii[this.textStartRow] + this.padTextForSpeechBubble(speechText[0]) + " .";
+        ownerAscii[this.textStartRow+1] = ownerAscii[this.textStartRow+1] + this.padTextForSpeechBubble(speechText[1]) + " .";
+        ownerAscii[this.textStartRow+2] = ownerAscii[this.textStartRow+2] + this.padTextForSpeechBubble(speechText[2]) + " .";
+    }
     render() {
+        this.addSpeechText(this.asciiOwner, this.props.speech);
         let itemsList = this.asciiOwner.map(function(item){
             return <span>{item}<br/></span>;
         });
